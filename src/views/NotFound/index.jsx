4a4dcf8f2941f0e5e13e32205aaa9c0595a43759
Sanya <sanyamasoudi9@gmail.com>
2025-03-13 21:54:40 +0330

    style: Enhance responsiveness NotFound page layout

--- src/views/NotFound/index.jsx
+++ src/views/NotFound/index.jsx
@@ -5,9 +5,11 @@ import LottieAnim from "../../components/LottieAnim";
 const NotFound = () => {
 	return (
 		<>
-			<div className="flex justify-center items-center flex-col">
-				<LottieAnim lottieJson={notFoundAnim} width={500} height={500} />
-                <h1 className="text-4xl font-bold text-white drop-shadow-lg tracking-wide">صفحه مورد نظر یافت نشد</h1>
+			<div className="flex justify-center items-center flex-col min-h-screen pb-20">
+				<div className="w-[500px] max-w-full px-4">
+					<LottieAnim lottieJson={notFoundAnim} width="100%" height="100%" />
+				</div>
+				<h1 className="text-2xl md:text-5xl font-bold text-white drop-shadow-lg tracking-wide text-center px-4">صفحه مورد نظر یافت نشد</h1>
 			</div>
 		</>
 	);
