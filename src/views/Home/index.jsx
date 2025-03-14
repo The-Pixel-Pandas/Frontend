186a36063bc488fc1fe312f881cae6d03d76dff3
Sanya <sanyamasoudi9@gmail.com>
2025-03-15 03:28:22 +0330

    feat: Add multiple font styles and add cursor image

--- src/views/Home/index.jsx
+++ src/views/Home/index.jsx
@@ -3,8 +3,20 @@ import React from "react";
 const Home = () => {
 	return (
 		<>
-			{/* test tailwind works correctly remove later */}
-			<div className="flex flex-auto text-white font-Lalezar">Home</div>
+			{/* see all font styles. remove later */}
+			<div className="flex flex-col flex-auto text-white justify-center items-center">
+				<div className="font-Lalezar">پانداهای پیکسلی</div>
+				<div className="font-InterMedium"> پانداهای پیکسلی</div>
+				<div className="font-MorabbaBlack">پانداهای پیکسلی</div>
+				<div className="font-MorabbaBold">پانداهای پیکسلی</div>
+				<div className="font-MorabbaExtraBold">پانداهای پیکسلی</div>
+				<div className="font-MorabbaHeavy">پانداهای پیکسلی</div>
+				<div className="font-MorabbaLight">پانداهای پیکسلی</div>
+				<div className="font-MorabbaMedium">پانداهای پیکسلی</div>
+				<div className="font-MorabbaRegular">پانداهای پیکسلی</div>
+				<div className="font-MorabbaSemiBold">پانداهای پیکسلی</div>
+				<div className="font-MorabbaUltraLight">پانداهای پیکسلی</div>
+			</div>
 		</>
 	);
 };
