d8a9e839259c68be166dd71a6b31c590be53a7e3
Sanya <sanyamasoudi9@gmail.com>
2025-03-20 18:22:29 +0330

    feat: Add Avatar and store for handle coin and avatar data

---
+++ src/services/stores/avatarStore/index.js
@@ -0,0 +1,22 @@
+import { create } from "zustand";
+import avatar1 from "../../../assets/images/Avatars/user01.png";
+import avatar2 from "../../../assets/images/Avatars/user02.png";
+import avatar3 from "../../../assets/images/Avatars/user03.png";
+import avatar4 from "../../../assets/images/Avatars/user04.png";
+import avatar5 from "../../../assets/images/Avatars/user05.png";
+import avatar6 from "../../../assets/images/Avatars/user06.png";
+import avatar7 from "../../../assets/images/Avatars/user07.png";	
+import avatar8 from "../../../assets/images/Avatars/user08.png";
+
+
+const avatars = [avatar1, avatar2, avatar3, avatar4, avatar5, avatar6, avatar7, avatar8];
+
+const useAvatarStore = create((set, get) => ({
+	avatars: avatars,
+	avatarNumber: 1,
+    setAvatarNumber: (avatarNumber) => set({ avatarNumber: avatarNumber }),
+    getAvatarNumber: () => get().avatarNumber,
+    getAvatarSrc: () => avatars[get().avatarNumber - 1],
+}));
+
+export default useAvatarStore;
