59d5d3e07e52682c21a7c7174cb8eac4e457c977
Sanya <sanyamasoudi9@gmail.com>
2025-03-27 03:36:48 +0330

    hmmm

--- src/components/SearchBar/index.jsx
+++ src/components/SearchBar/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import searchIcon from "../../assets/images/SearchIcon.png";
+import { Autocomplete, TextField } from "@mui/material";
 
 const SearchBar = ({ width = "535px" }) => {
 	const searchEvent = (e) => {
@@ -17,13 +18,58 @@ const SearchBar = ({ width = "535px" }) => {
 							<img src={searchIcon} alt="SearchIcon" />
 						</button>
 					</div>
-					<input
-						type="search"
-						className="relative m-0 block w-full rounded-md b bg-white bg-clip-padding px-3 py-[0.25rem] pr-10 text-base font-normal leading-[1.6] text-surface outline-none transition duration-200 ease-in-out placeholder:text-gray/400  motion-reduce:transition-none font-MorabbaRegular"
-						placeholder=" جستجوی سوال ها براساس عنواین و تگ ها ..."
-						aria-label="Search"
-						id="exampleFormControlInput2"
-						dir="rtl"
+					<Autocomplete
+						options={[
+							"سوال 1",
+							"سوال 2",
+							"سوال 3",
+							"سوال 4",
+							"سوال 5",
+							"سوال 6",
+							"سوال 7",
+							"سوال 8",
+							"سوال 9",
+							"سوال 10",
+							"سوال 11",
+							"سوال 12",
+							"سوال 13",
+							"سوال 14",
+							"سوال 15",
+							"سوال 16",
+							"سوال 17",
+							"سوال 18",
+							"سوال 19",
+							"سوال 20",
+						]}
+						renderInput={(params) => (
+							<TextField
+								{...params}
+								type="search"
+								className="relative m-0 block w-full rounded-md bg-white bg-clip-padding"
+								placeholder=" جستجوی سوال ها براساس عنواین و تگ ها ..."
+								aria-label="Search"
+								id="exampleFormControlInput2"
+								dir="rtl"
+								variant="outlined"
+								sx={{
+									'& .MuiOutlinedInput-root': {
+										padding: '0.25rem',
+										'& fieldset': {
+											borderRadius: '0.375rem',
+										},
+									},
+									'& .MuiInputBase-input': {
+										fontFamily: 'MorabbaRegular',
+										fontSize: '1rem',
+										lineHeight: 1.6,
+										padding: '0 0.75rem',
+										'&::placeholder': {
+											color: 'rgba(0, 0, 0, 0.4)',
+										},
+									},
+								}}
+							/>
+						)}
 					/>
 				</div>
 			</div>
