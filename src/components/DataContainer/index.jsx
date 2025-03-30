f7242b9d37aa3568f2e7143b5ea01f89e28171ae
Sanya <sanyamasoudi9@gmail.com>
2025-03-30 07:26:34 +0330

    completed question page data box

--- src/components/DataContainer/index.jsx
+++ src/components/DataContainer/index.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import PropTypes from "prop-types";
 import databox from "../../assets/images/databox.png";
 import dataCategory from "../../assets/images/dataCategory.png";
+import clock from "../../assets/images/clock.png";
+import coinLogo from "../../assets/images/coinLogo.png";
 
 const DataContainer = ({
 	width,
@@ -10,30 +12,72 @@ const DataContainer = ({
 	description,
 	image,
 	categories,
+	numberOfVisits,
+	coins,
+	date,
 }) => {
 	return (
-		<>
+		<div className="flex justify-center items-center w-full">
 			<div className="relative" style={{ width, height }}>
-				<img src={databox} alt="dataContainer" className="absolute inset-0" />
-				<div className="absolute inset-0 top-0 right-0 mr-10 mt-10" dir="rtl">
-					<img
-						src={image}
-						alt="sample"
-						className="absolute inset-0"
-						style={{ width: 80, height: 80 }}
-					/>
-					<div className="absolute inset-0 top-0 right-0 mt-6 mr-24">
-						<div className="text-white  text-3xl font-Lalezar">{title}</div>
-						<div
-							className="text-white  text-2xl font-MorabbaRegular mt-5"
-							style={{ width: "80%" }}
+				<img
+					src={databox}
+					alt="dataContainer"
+					className="absolute inset-0 w-full h-full"
+				/>
+
+				{/* Content Container */}
+				<div className="absolute inset-0 flex flex-row p-10" dir="rtl">
+					{/* Image */}
+					<div className="flex-shrink-0">
+						<img src={image} alt="sample" className="w-20 h-20 object-cover" />
+					</div>
+
+					{/* Text Content */}
+					<div className="flex-grow pr-6">
+						<p className="text-white text-3xl font-Lalezar">{title}</p>
+						<p
+							className="text-white text-2xl font-MorabbaMedium mt-5"
+							style={{ width: "93%" }}
 						>
 							{description}
+						</p>
+						<p className="text-white text-lg font-MorabbaRegular absolute top-5 left-10">
+							{numberOfVisits.toLocaleString("fa")} &nbsp; بازدید
+						</p>
+						<div className="flex flex-row gap-4 items-center absolute bottom-0 left-10 mb-8">
+							<div className="flex flex-row gap-0 items-center">
+								<img src={coinLogo} alt="coinLogo" className="w-14 h-12" />
+								<p className="text-white text-lg font-MorabbaRegular">
+									{coins.toLocaleString("fa")} &nbsp; پاندا کوین
+								</p>
+							</div>
+							<div className="flex flex-row gap-2 items-center">
+								<img src={clock} alt="clock" className="w-5 h-5" />
+								<p className="text-white text-lg font-MorabbaRegular">
+									{date.toLocaleString("fa")}
+								</p>
+							</div>
 						</div>
+					</div>
+
+					{/* Categories */}
+					<div className="absolute bottom-10 right-10 flex flex-row gap-2 mr-10">
+						{categories.map((category, index) => (
+							<div key={index} className="relative">
+								<img
+									src={dataCategory}
+									alt="dataCategory"
+									className="w-30 h-8"
+								/>
+								<div className="absolute inset-0 flex items-center justify-center text-white text-lg font-MorabbaRegular whitespace-nowrap">
+									{category}
+								</div>
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
-		</>
+		</div>
 	);
 };
 
@@ -44,6 +88,9 @@ DataContainer.propTypes = {
 	description: PropTypes.string.isRequired,
 	image: PropTypes.string.isRequired,
 	categories: PropTypes.array.isRequired,
+	numberOfVisits: PropTypes.number.isRequired,
+	coins: PropTypes.number.isRequired,
+	date: PropTypes.string.isRequired,
 };
 
 export default DataContainer;
