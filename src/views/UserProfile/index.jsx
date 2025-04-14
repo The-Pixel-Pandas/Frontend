213995fabb66c04b25e1964583fcbfd2f328b044
Fateme Alizade <ftmwnerd.wrk@gmail.com>
2025-04-14 16:40:33 +0330

    delete headerbox

--- src/views/UserProfile/index.jsx
+++ src/views/UserProfile/index.jsx
@@ -14,22 +14,6 @@ const UserProfile = () => {
 
 	return (
 		<>
-			{/* Header */}
-			<div className="flex flex-col absolute gap-10 top-1/2 left-1/2">
-				<div className="flex flex-row gap-5 ml-17 mt-20">
-					<div className="flex flex-col  gap-4 " dir="rtl">
-						<div className="text-4xl font-MorabbaMedium text-white">
-							نام کاربری
-						</div>
-						<div className="text-lg font-MorabbaMedium text-white">
-							توضیحات (بیوگرافی)
-						</div>
-					</div>
-					<div className="flex flex-col gap-4 " dir="rtl">
-						<UserAvatar width={100} height={100} />
-					</div>
-				</div>
-			</div>
 			<div className="absolute">
 				{/* Medal Container */}
 				<div className="top-1/2 left-0  mt-48 ml-40">
