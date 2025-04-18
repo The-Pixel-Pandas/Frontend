ab9a3851c4d1b480adb9f34e600216403fcb3e48
Sanya <sanyamasoudi9@gmail.com>
2025-04-18 13:59:38 +0330

    feat: implement avatar selector

--- src/views/UserInfo/index.jsx
+++ src/views/UserInfo/index.jsx
@@ -1,7 +1,73 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import avatarInfoContainer from "../../assets/images/avatarInfoContainer.png";
+import avatarBorder from "../../assets/images/avatarBorder.png";
+import { useAvatarStore } from "../../services";
 
 const UserInfo = () => {
-	return <></>;
+	const { avatars, setAvatarNumber, getAvatarNumber } = useAvatarStore();
+	const [selectedAvatar, setSelectedAvatar] = useState(0);
+
+	const handleAvatarSelect = (index) => {
+		setSelectedAvatar(index);
+		setAvatarNumber(index + 1);
+	};
+
+	useEffect(() => {
+		setSelectedAvatar(getAvatarNumber() - 1);
+	}, [getAvatarNumber()]);
+
+	return (
+		<>
+			<div className="absolute inset-0 left-1/2 -translate-x-1/2 mt-16 ml-40">
+				{/* Avatar Selector */}
+				<div className="flex flex-col gap-0.5">
+					{/* Title */}
+					<div className="text-white font-MorabbaMedium text-lg ml-72">
+						آواتار موردنظر را انتخاب کنید
+					</div>
+
+					{/* Avatars */}
+					<div className="relative">
+						{/* Container */}
+						<img
+							src={avatarInfoContainer}
+							alt="avatarInfoContainer"
+							className="w-[500px] h-[100px]"
+						/>
+
+						{/* Avatar scrollable container */}
+						<div className="absolute inset-0 left-6 top-3 right-10 pr-2 ">
+							<div
+								className="flex flex-row gap-5 overflow-x-auto w-[460px] pb-2 no-scrollbar  "
+								dir="rtl"
+							>
+								{avatars.map((avatar, index) => (
+									<div
+										key={index}
+										className={`relative flex-shrink-0 cursor-pointer ${selectedAvatar === index ? "opacity-100" : "opacity-80"}`}
+										onClick={() => handleAvatarSelect(index)}
+									>
+										<img
+											src={avatar}
+											alt={`avatar-${index + 1}`}
+											className="w-[76px] h-[76px] object-contain"
+										/>
+										<div className="absolute inset-0">
+											<img
+												src={avatarBorder}
+												alt="avatarBorder"
+												className={`w-[75px] h-[76px] object-contain ${selectedAvatar === index ? "opacity-100" : "opacity-50"}`}
+											/>
+										</div>
+									</div>
+								))}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default UserInfo;
