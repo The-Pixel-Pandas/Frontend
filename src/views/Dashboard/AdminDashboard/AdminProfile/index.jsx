102dd848f0361eb9ebe36c362fcbf5b123117b0c
Sanya <sanyamasoudi9@gmail.com>
2025-05-17 22:40:10 +0330

    feat: Implement profile for admin and user dashboard

--- src/views/Dashboard/AdminDashboard/AdminProfile/index.jsx
+++ src/views/Dashboard/AdminDashboard/AdminProfile/index.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Profile } from "../../../../components";
 
 const AdminProfile = () => {
-	return <></>;
+	return <Profile />;
 };
 
 export default AdminProfile;
