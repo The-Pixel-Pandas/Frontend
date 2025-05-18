a9f8f925eb521b1de12708d252b4300238bb09aa
Fateme Alizade <ftmwnerd.wrk@gmail.com>
2025-05-18 16:42:18 +0330

    feat: Implement notification page

---
+++ src/components/dashboard/NotificationBox/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import bilbilak from "../../../assets/images/bilbilak.png";
+
+const NotificationBox = ({ title, description, background }) => {
+	return (
+		<div>
+			<div className=" relative">
+				<img
+					src={background}
+					alt="blueBox"
+					style={{ width: 950, height: 100 }}
+				/>
+				<div className="absolute inset-0 left-0 top-0 ">
+					<div className="flex flex-col gap-2 mt-5 mr-5 " dir="rtl">
+						<div className=" font-MorabbaBold whitespace-nowrap text-xl text-white">
+							{title}
+						</div>
+						<div className="flex flex-row items-center gap-2">
+							<img src={bilbilak} alt="bilbilak" />
+							<div className=" font-MorabbaMedium whitespace-nowrap text-lg text-white">
+								{description}
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default NotificationBox;
---
+++ src/views/Dashboard/UserDashboard/Notification/index.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+import { NotificationBox } from "../../../../components";
+import bell from "../../../../assets/images/bell.png";
+import adminContainer from "../../../../assets/images/adminContainer.png";
+import blueBox from "../../../../assets/images/blueBox.png";
+import purpleBox from "../../../../assets/images/purpleBox.png";
+
+const Notification = () => {
+	return (
+		<>
+			<div className="absolute left-0 top-0 flex items-center z-0 ml-14">
+				<div className="relative">
+					{/* BackGround Image */}
+					<img
+						src={adminContainer}
+						alt="adminContainer"
+						style={{ width: 1100, height: 600 }}
+						className="mt-10"
+					/>
+					{/* Header */}
+					<div className="absolute inset-0 top-0 left-1/2 ml-16 mt-20">
+						<div className=" flex flex-row items-center ">
+							<div className=" flex flex-col gap-1 mr-2.5 " dir="rtl">
+								<div className=" font-MorabbaBold whitespace-nowrap text-2xl text-white">
+									اعلان ها و پیام ها
+								</div>
+								<div className=" font-MorabbaMedium whitespace-nowrap text-xl text-white">
+									مدیریت اعلان ها ، پیام ها و اطلاعیه های سیستم
+								</div>
+							</div>
+							<img src={bell} alt="bell" style={{ width: 90, height: 90 }} />
+						</div>
+					</div>
+					{/* Notifications */}
+					<div className="absolute inset-0 left-0 top-0 flex items-center z-50 ml-14 mt-44">
+						<div className="flex flex-col items-center h-[400px] gap-10 overflow-y-scroll no-scrollbar ">
+							<NotificationBox
+								title="یک خبر جدید ثبت شد"
+								description="طوفان امروز چه مقدار مخرب است؟"
+								background={blueBox}
+							/>
+							<NotificationBox
+								title="یک خبر جدید ثبت شد"
+								description="طوفان امروز چه مقدار مخرب است؟"
+								background={purpleBox}
+							/>
+							<NotificationBox
+								title="یک خبر جدید ثبت شد"
+								description="طوفان امروز چه مقدار مخرب است؟"
+								background={blueBox}
+							/>
+							<NotificationBox
+								title="یک خبر جدید ثبت شد"
+								description="طوفان امروز چه مقدار مخرب است؟"
+								background={purpleBox}
+							/>
+							<NotificationBox
+								title="یک خبر جدید ثبت شد"
+								description="طوفان امروز چه مقدار مخرب است؟"
+								background={blueBox}
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+
+export default Notification;
